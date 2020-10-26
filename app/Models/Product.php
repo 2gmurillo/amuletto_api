5a2ff0fee367e0b7d044e50f658641354b4a92be
2gmurillo <2gmurillo@gmail.com>
2020-10-26 15:08:57 -0500

    feat: initial configutarion
    - Category (model, migration, factory, seeder and api controller)
    - Product (model, migration, factory, seeder and api controller)
    - Relationships between categories and products
    - .env (database configuration with mysql)
    - testing (stub template)

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'photo',
+        'price',
+        'description',
+        'stock',
+        'status',
+        'category_id',
+    ];
+
+    /**
+     * Get the category that owns the product.
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
